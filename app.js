9e3da0ea3732a7fe5be8e7c229ace6c0edabfb12
Duane Rush <desertduane@yahoo.com>
2017-07-17 13:37:44 -0700

    setup done

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+var firstPike = {
+  name: '1st and Pike',
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCookiesPerSale: 6.3,
+  openHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']
+
+}
